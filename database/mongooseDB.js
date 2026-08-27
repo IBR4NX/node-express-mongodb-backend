8f8e7d1fa32181f0687979a8e0fba5e700998a73
Ibrahim Dabwan Al Mekhlafi <ibrahim.al.mekhlafi.my@gmail.com>
2026-08-27 19:04:10 +0300

    Refactor MongoDB connection to use DB_URL from config

--- database/mongooseDB.js
+++ database/mongooseDB.js
@@ -1,7 +1,3 @@
-//  mongodb+srv://thrivq_db_ibovd:OFmfyK8Ddpa06ukX@ibovsdb.coaq32q.mongodb.net/?appName=ibovsDB
-// mongoose.connect('mongodb+srv://thrivq_db_ibovd:OFmfyK8Ddpa06ukX@ibovsdb.coaq32q.mongodb.net/?appName=ibovsDB')
-// const databaseurl =`mongodb+srv://Ibrahim:Ibr.714339227@cluster0.utsd0uz.mongodb.net/?appName=Cluster0`;
-// const uri ="mongodb+srv://ibovstest:ibovs12345@ibr4nx.sjth1pb.mongodb.net/?appName=ibr4nx";
 import mongoose from"mongoose";
 import {DB_URL} from'../config.js';
 mongoose
